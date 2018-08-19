8693eab358da66c3db4f3cb4c76f8b2e9689abe8
Syed Sajid <syedsajid341@gmail.com>
2018-08-19 13:25:44 +0000

    Pixys pie

--- pixys_sanders.mk
+++ pixys_sanders.mk
@@ -19,7 +19,7 @@
 $(call inherit-product, device/motorola/sanders/full_sanders.mk)
 
 # Inherit some common SacredOS stuff.
-$(call inherit-product, vendor/sacred/common.mk)
+$(call inherit-product, vendor/pixys/common.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -27,7 +27,7 @@ TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
-PRODUCT_NAME := sacred_sanders
+PRODUCT_NAME := pixys_sanders
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
